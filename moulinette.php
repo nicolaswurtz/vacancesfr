b3609ae39146b7a8d49edc4e9196cb2c335d0e37
Nicolas Wurtz <6003856+nicolaswurtz@users.noreply.github.com>
2018-03-02 22:04:15 +0100

    Correction quand pas de date de rentrée prévue l'été

--- moulinette.php
+++ moulinette.php
@@ -35,15 +35,17 @@ foreach ($json as $evenement) {
                 if ($titre == "Vacances d\'été") {
                     $fin = $fin_de_l_ete[$zone.substr($debut,0,4)];
                 }
-                $liste[] = array(
-                    'titre'         => $titre,
-                    'zone'          => $zone,
-                    'departement'   => $dep['num_dep'],
-                    'academie'      => $academie,
-                    'debut'         => $debut,
-                    'fin'           => $fin
-                );
-                $csv .= $zone.','.$academie.','.$dep['num_dep'].','.$debut.','.$fin.','.$titre."\n";
+                if (!empty(*fin)) {
+                    $liste[] = array(
+                        'titre'         => $titre,
+                        'zone'          => $zone,
+                        'departement'   => $dep['num_dep'],
+                        'academie'      => $academie,
+                        'debut'         => $debut,
+                        'fin'           => $fin
+                    );
+                    $csv .= $zone.','.$academie.','.$dep['num_dep'].','.$debut.','.$fin.','.$titre."\n";
+                }
             }
         }
     }
